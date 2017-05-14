3dd75d212baaf354abb76876f37afb47367706c9
Ben Smith <ben@10consulting.com>
2017-05-14 22:36:52 +0100

    Include Hex package details in `mix.exs`

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Commanded.EventStore.Adapters.EventStore.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.4",
       description: description(),
+      package: package(),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
